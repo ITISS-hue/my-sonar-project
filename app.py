cc4cf63a9c634de0144b87ec9960c5355b95f9c2
ITISS-hue <cdacbatch@gmail.com>
2026-07-06 23:12:22 +0530

    first project

---
+++ app.py
@@ -0,0 +1,17 @@
+import sqlite3, os, pickle
+
+# SQL Injection
+def get_user(username):
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    query = f"SELECT * FROM users WHERE username = '{username}'"  # Vulnerable
+    cursor.execute(query)
+    return cursor.fetchall()
+
+# Command Injection
+def run_ping(host):
+    os.system(f"ping -c 1 {host}")  # Vulnerable
+
+# Insecure Deserialization
+def load_data(data):
+    return pickle.loads(data)  # Vulnerable
